9141fd414c3dbbaec52bf966aed5cd6cc3f80d53
Adriano Fernandes <djadrianof@gmail.com>
2012-10-10 00:27:24 -0300

    update structure

---
+++ source/public/javascripts/app.js
@@ -0,0 +1,17 @@
+// ---------------------------
+// Create Namespace
+// ---------------------------
+
+window.EURODANCECOMBR = {};
+
+// ---------------------------
+// Import javascripts to app
+// ---------------------------
+
+//= require_self
+//= require "_vendors/core/underscore"
+//= require "_vendors/core/json2"
+//= require "_vendors/core/jquery"
+//= require "_vendors/core/backbone"
+//= require "_vendors/shared/mixins"
+//= require "initializer"
